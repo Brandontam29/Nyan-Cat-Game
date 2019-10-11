2e0daf0744d52ce247d5b179e2ea85b5787defa4
Brandon Tam <brandon.tam29@gmail.com>
2019-10-11 04:44:10 -0400

    game now runs but still buggy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let gameEngine = new Engine(document.getElementById('status')) 
-let keydownHandler = event => { 
-    if (event.code === "ArrowLeft") { 
-        gameEngine.player.moveLeft() 
-    } 
-    if (event.code === "ArrowRight") { 
-        gameEngine.player.moveRight() 
-    } 
-} 
-document.addEventListener("keydown", keydownHandler) 
+let gameEngine = new Engine(document.getElementById("status"))
+let keydownHandler = event => {
+  if (event.code === "ArrowLeft") {
+    gameEngine.player.moveLeft()
+  }
+  if (event.code === "ArrowRight") {
+    gameEngine.player.moveRight()
+  }
+}
+document.addEventListener("keydown", keydownHandler)
 gameEngine.gameLoop()
